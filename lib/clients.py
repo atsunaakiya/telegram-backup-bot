759496bca7233613a616cf261deb142b1f379e93
AtsunaAkiya <atsunaakiya@outlook.com>
2021-06-28 00:50:15 +0800

    debug

--- lib/clients.py
+++ lib/clients.py
@@ -1,15 +1,14 @@
-from telegram.ext import Updater
-from webdav3.client import Client
-
 from lib.conf import load_config, TOKEN, TELEGRAM, WEBDAV, HOST, USERNAME, PASSWORD
 
 
-def create_bot() -> Updater:
+def create_bot():
+    from telegram.ext import Updater
     config = load_config()
     return Updater(config[TELEGRAM][TOKEN])
 
 
 def create_dav():
+    from webdav3.client import Client
     config = load_config()
     return Client({
         'webdav_hostname': config[WEBDAV][HOST],
